719b5270d9cd4625b97823f0be2b784ef8a52bfb
JuliDi <20155974+JuliDi@users.noreply.github.com>
2024-04-11 11:00:01 +0200

    update and fix input_heartrate

--- projects/inputs/input_heartrate/input_heartrate.ino
+++ projects/inputs/input_heartrate/input_heartrate.ino
@@ -1,38 +1,15 @@
-// Source https://github.com/gabriel-milan/Arduino-MAX30100
-
-/*
-Arduino-MAX30100 oximetry / heart rate integrated sensor library
-Copyright (C) 2016  OXullo Intersecans <x@brainrapers.org>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+// Originalquelle https://github.com/gabriel-milan/Arduino-MAX30100
 
 #include <Wire.h>
 #include "MAX30100_PulseOximeter.h"
 
-#define REPORTING_PERIOD_MS     1000
-
-// PulseOximeter is the higher level interface to the sensor
-// it offers:
-//  * beat detection reporting
-//  * heart rate calculation
-//  * SpO2 (oxidation level) calculation
+// Pulsoximeter
 PulseOximeter pox;
 
-uint32_t tsLastReport = 0;
+// Zeitvariable, mit der unten festgestellt wird, ob ein Statustext ausgegeben werden soll
+uint32_t time_last = 0;
 
-// Callback (registered below) fired when a pulse is detected
+// Callback (wird weiter unten gesetzt), das bei jedem erkannten Herzschlag aufgerufen wird
 void onBeatDetected()
 {
     Serial.println("Beat!");
@@ -42,48 +19,37 @@ void setup()
 {
     Serial.begin(115200);
 
-    Serial.println("Initializing pulse oximeter..");
-    delay(500);
-    Serial.println("pox begin");
-    delay(1000);
-    // Initialize the PulseOximeter instance
-    // Failures are generally due to an improper I2C wiring, missing power supply
-    // or wrong target chip
+    Serial.print("Initializing pulse oximeter..");
+
+    // Initialisieren. Wenn alles richtig verbunden ist, wird man hier meist keinen Output sehen.
     if (!pox.begin()) {
         Serial.println("FAILED");
-        Serial.flush();
-        delay(1000);
-        //for(;;);
+        return;
     } else {
         Serial.println("SUCCESS");
     }
 
-        delay(500);
-    // The default current for the IR LED is 50mA and it could be changed
-    //   by uncommenting the following line. Check MAX30100_Registers.h for all the
-    //   available options.
-    // pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
+    // Den Strom der Infrarot LED auf 27,1 mA setzen verbessert die Herzschlagerkennung ein bisschen
+    pox.setIRLedCurrent(MAX30100_LED_CURR_27_1MA);
 
-    // Register a callback for the beat detection
-    //pox.setOnBeatDetectedCallback(onBeatDetected);
+    // Callback registrieren
+    pox.setOnBeatDetectedCallback(onBeatDetected);
 }
 
 void loop()
 {
-    Serial.println("loop!");
-    // Make sure to call update as fast as possible
+    // So oft wie moeglich aufrufen
     pox.update();
-    Serial.println("loop!");
 
-    // Asynchronously dump heart rate and oxidation levels to the serial
-    // For both, a value of 0 means "invalid"
-    if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
-        Serial.print("Heart rate:");
+    // Puls und Sauerstoffsaettigung werden alle 1000 ms ueber das serielle Interface ausgegeben.
+    // Wenn der Wert 0 ist, ist die Messung fehlgeschlagen.
+    if (millis() - time_last > 1000) {
+        Serial.print("Puls: ");
         Serial.print(pox.getHeartRate());
-        Serial.print("bpm / SpO2:");
+        Serial.print("bpm / SpO2: ");
         Serial.print(pox.getSpO2());
-        Serial.println("%");
+        Serial.println(" %");
 
-        tsLastReport = millis();
+        time_last = millis();
     }
 }
